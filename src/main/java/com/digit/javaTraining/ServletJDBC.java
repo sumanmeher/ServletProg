1d87c890c55af97ba53199a0905f52dc979748b7
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-07-31 17:31:46 +0530

    new code

---
+++ src/main/java/com/digit/javaTraining/ServletJDBC.java
@@ -0,0 +1,18 @@
+package com.digit.javaTraining;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletJDBC")
+public class ServletJDBC extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+}
